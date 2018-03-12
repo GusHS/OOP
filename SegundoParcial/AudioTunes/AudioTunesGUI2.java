9e7a1ce154d0d9caadd1098d209f3a48bbe085af
GusHS <gustavohdzs@hotmail.com>
2018-03-12 08:24:55 -0600

    added new project

---
+++ SegundoParcial/AudioTunes/AudioTunesGUI2.java
@@ -0,0 +1,86 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class AudioTunesGUI2 extends JFrame implements ActionListener{
+    //Atributos de la aplicación
+    private JTextField  tfArtista, tfAlbum, tfSong;
+    private JTextArea   taArtistas, taAlbums, taSongs;
+    private JButton     bCatalogo, bArtista, bAlbums, bSongs;
+    private JButton     bPlay, bStop;
+    private JPanel      panelUsuario, panelArtistas, panelAlbums;
+    private JPanel      panelSongs, panelAudio, panelPrincipal;
+    
+    public AudioTunesGUI2(){
+        super("Audio Tunes");
+
+        //1. Crear Objetos de los Atributos.
+        tfArtista   = new JTextField(10);
+        tfAlbum     = new JTextField(10);
+        tfSong      = new JTextField(10);
+        taArtistas = new JTextArea("Artistas");
+        taAlbums   = new JTextArea("Albums");
+        taSongs    = new JTextArea("Songs");
+        bCatalogo = new JButton("Catalogo");
+        bArtista  = new JButton("Artista");
+        bAlbums   = new JButton("Albums");
+        bSongs    = new JButton("Songs");
+        bPlay     = new JButton("Play");
+        bStop     = new JButton("Stop");
+        panelUsuario   = new JPanel();
+        panelArtistas  = new JPanel();
+        panelAlbums    = new JPanel();
+        panelSongs     = new JPanel();
+        panelAudio     = new JPanel();
+        panelPrincipal = new JPanel();
+
+        bPlay.addActionListener(this);
+        bStop.addActionListener(this);
+        
+        //2. Definir Layouts de los JPanels.
+        panelUsuario.setLayout(new FlowLayout());
+        panelArtistas.setLayout(new GridLayout(1,1));
+        panelAlbums.setLayout(new GridLayout(1,1));
+        panelSongs.setLayout(new GridLayout(1,1));
+        panelAudio.setLayout(new FlowLayout());
+        panelPrincipal.setLayout(new BorderLayout());
+
+        //3. Colocar los objetos de los atributos en los panels correspondientes
+        panelUsuario.add(bCatalogo);
+        panelUsuario.add(new JLabel("Artista: "));
+        panelUsuario.add(tfArtista);
+        panelUsuario.add(bAlbums);
+        panelUsuario.add(new JLabel("Albums: "));
+        panelUsuario.add(tfAlbum);
+        panelUsuario.add(new JLabel("Songs: "));
+        panelUsuario.add(bSongs);
+
+        panelArtistas.add(new JScrollPane(taArtistas));
+        panelAlbums.add(new JScrollPane(taAlbums));
+        panelSongs.add(new JScrollPane(taSongs));
+
+        panelAudio.add(new JLabel("Song: "));
+        panelAudio.add(tfSong);
+        panelAudio.add(bPlay);
+        panelAudio.add(bStop);
+
+        panelPrincipal.add(panelUsuario, BorderLayout.NORTH);
+        panelPrincipal.add(panelArtistas, BorderLayout.WEST);
+        panelPrincipal.add(panelAlbums, BorderLayout.CENTER);
+        panelPrincipal.add(panelSongs, BorderLayout.EAST);
+        panelPrincipal.add(panelAudio, BorderLayout.SOUTH);
+
+        //4. Adicionar panel principal al JFrame.
+        add(panelPrincipal);
+        setSize(700,500);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void ActionPerformed(ActionEvent e) {
+      
+    }
+    public static void main(String[] args) {
+        new AudioTunesGUI2();
+    }
+}
